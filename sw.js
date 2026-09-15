2fe1e4b339f16aefc3084fbc522412d8a7765a02
reversoworks-gestor <3.21663563e+08+reversoworks-gestor@users.noreply.github.com>
2026-09-15 20:29:38 +0000

    deploy: publish mobile search modal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "reverso-works-v7";
+const CACHE = "reverso-works-v8";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
